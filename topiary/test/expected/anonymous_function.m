1ee2c2b2c9234dd21cec7fa82e98c3908c793be0
Edgar Costa <edgarc@mit.edu>
2026-05-08 19:52:48 -0400

    fix(topiary): handle anonymous function/procedure expressions
    When function or procedure is used as an expression (no name field),
    suppress the trailing space after the keyword and the leading
    hardline before the definition. Named definitions are unchanged.
    
    Before: f :=\nfunction (x) ... end function;
    After:  f := function(x) ... end function;

---
+++ topiary/test/expected/anonymous_function.m
@@ -0,0 +1,12 @@
+f := function(x)
+    return x;
+end function;
+g := function(a, b)
+    return a + b;
+end function;
+p := procedure(~x)
+    x +:= 1;
+end procedure;
+q := Apply(function(x)
+    return x + 1;
+end function, [1, 2, 3]);
